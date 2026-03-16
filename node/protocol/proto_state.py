4e95d463bd1ac7d542f17bf4ff78f0b7dce4d6ab
Diego Taveras <diegoat2@illinois.edu>
2026-03-16 18:22:17 -0500

    adding proto_state

---
+++ node/protocol/proto_state.py
@@ -0,0 +1,20 @@
+class ProtocolState:
+    def __init__(
+        self,
+        node_id: str,
+        peers: list[str],
+        role: str = "replica",
+        f: int = 1,
+        current_view: int = 0,
+        primary_id = "1",
+    ):
+
+        self.node_id = str(node_id)
+        self.peers = list(peers)
+        self.role = role  # "replica" or "client"
+        self.f = f
+        self.n = len(peers)
+        self.current_view = current_view
+
+        self.primary_id = primary_id if primary_id is not None else "1"
+    
