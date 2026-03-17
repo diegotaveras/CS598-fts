2905fac8884977ac2e286945446dc3405643965f
Diego Taveras <diegoat2@illinois.edu>
2026-03-17 15:45:27 -0500

    adding history digest logic and ordered_history field

--- node/protocol/proto_state.py
+++ node/protocol/proto_state.py
@@ -20,6 +20,7 @@ class ProtocolState:
         self.current_view = current_view
         self.seqnum = 0
         self.history_digest = "GENESIS"
+        self.ordered_history = []
         self.primary_id = primary_id if primary_id is not None else "1"
 
     def allocate_seqno(self) -> int:
@@ -30,4 +31,13 @@ class ProtocolState:
         material = f"{self.history_digest}:{request_digest}".encode("utf-8")
         self.history_digest = hashlib.sha256(material).hexdigest()
         return self.history_digest
+
+    def expected_history_digest(self, request_digest: str) -> str:
+        material = f"{self.history_digest}:{request_digest}".encode("utf-8")
+        return hashlib.sha256(material).hexdigest()
+
+    def append_ordered_request(self, ordered_request):
+        self.seqnum = ordered_request.seqno
+        self.history_digest = ordered_request.history_digest
+        self.ordered_history.append(ordered_request)
     
